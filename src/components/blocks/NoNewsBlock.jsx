8db0b26608e413379609b405ebf5d65f831a6431
Julia Volkova <juliavolk96@gmail.com>
2024-03-19 14:05:28 +0400

    init: initial commit

---
+++ src/components/blocks/NoNewsBlock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './css/NoNewsBlock.css'
+
+const NoNewsBlock = () => {
+  return(
+    <div className="no-news-container">
+      <p>На данный момент новостей нет. Пожалуйста, проверьте позже.</p>
+    </div>
+  );
+}
+
+export default NoNewsBlock;
